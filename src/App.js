9cbb4759fa7412601d242cc82b331d6469d9b392
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-08 19:59:56 +0800

    project route/page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home/Home'
+import Projects from './components/Projects/Projects'
 import Footer from './components/Footer'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
@@ -11,6 +12,7 @@ export default function App() {
         <Navbar />
           <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/projects' element={<Projects />} />
           </Routes>
         <Footer />
       </Router>
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Proj1 from '../../assets/proj_1.jpg'
+import Proj2 from '../../assets/proj_2.jpg'
+import Confidential from '../../assets/confidential.jpg'
+
+export default function Projects() {
+  return (
+    <div className='h-[95vh]'>
+        <h1 className='text-center text-white text-[20px] font-medium mt-[5vh]' style={{letterSpacing:"3px"}}>PROJECTS</h1>
+        <div className='flex my-16 px-32 justify-center flex-wrap'>
+            <div className='flex flex-col justify-center items-center w-[30%] my-4'>
+                <img src={Proj1} alt="" className='h-[180px] w-[285px] rounded-xl' style={{filter: "drop-shadow(7px 6px 9px rgba(255, 215, 58, 0.15))"}}/>
+                <h1 className='mt-3 text-white text-[13px] font-medium'>LGU Website</h1>
+                <button className='mt-2 px-2 py-[2px] bg-[#FFD63C] rounded text-[12px] font-semibold'>Details</button>
+            </div>
+            <div className='flex flex-col justify-center items-center w-[30%] my-4'>
+                <img src={Proj2} alt="" className='h-[180px] w-[285px] rounded-xl' style={{filter: "drop-shadow(7px 6px 9px rgba(255, 215, 58, 0.15))"}}/>
+                <h1 className='mt-3 text-white text-[13px] font-medium'>Personal Yoga Website</h1>
+                <button className='mt-2 px-2 py-[2px] bg-[#FFD63C] rounded text-[12px] font-semibold'>Details</button>
+            </div>
+            <div className='flex flex-col justify-center items-center w-[30%] my-4'>
+                <img src={Confidential} alt="" className='h-[180px] w-[285px] rounded-xl' style={{filter: "drop-shadow(7px 6px 9px rgba(255, 215, 58, 0.15))"}}/>
+                <h1 className='mt-3 text-white text-[13px] font-medium'>Inventory and A.R.E Monitoring System</h1>
+                <button className='mt-2 px-2 py-[2px] bg-[#FFD63C] rounded text-[12px] font-semibold'>Details</button>
+            </div>
+            <div className='flex flex-col justify-center items-center w-[30%] my-4'>
+                <img src={Confidential} alt="" className='h-[180px] w-[285px] rounded-xl' style={{filter: "drop-shadow(7px 6px 9px rgba(255, 215, 58, 0.15))"}}/>
+                <h1 className='mt-3 text-white text-[13px] font-medium'>Education Program</h1>
+                <button className='mt-2 px-2 py-[2px] bg-[#FFD63C] rounded text-[12px] font-semibold'>Details</button>
+            </div>
+            <div className='flex flex-col justify-center items-center w-[30%] my-4'>
+                <img src={Confidential} alt="" className='h-[180px] w-[285px] rounded-xl' style={{filter: "drop-shadow(7px 6px 9px rgba(255, 215, 58, 0.15))"}}/>
+                <h1 className='mt-3 text-white text-[13px] font-medium'>City Risk Operating System</h1>
+                <button className='mt-2 px-2 py-[2px] bg-[#FFD63C] rounded text-[12px] font-semibold'>Details</button>
+            </div>
+        </div>
+    </div>
+  )
+}
