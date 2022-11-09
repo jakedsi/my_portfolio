14e71130c3c95c66339cb3a5774169667cfe3eb0
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-09 13:24:37 +0800

    navlinks

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -8,26 +8,26 @@ export default function Hero() {
         <div className='h-[70vh] w-screen bg-[#6905F9] rounded-tr-[12rem] rounded-bl-[12rem]' style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/backgroundHero.png'})`,
             backgroundPositionY: 'center',
             backgroundPositionX: 'center'}}></div>
-        <div className='absolute top-0 w-screen flex justify-between gap-10'>
+        <div className='absolute top-0 w-screen flex justify-between gap-10 mt-[8vh]'>
             <div className='flex justify-between w-[55%]'>
             <div className='px-5'>
-                <button className='flex items-center text-white gap-2 mt-[10vh] text-[11px] bg-[#28045C] py-1 px-3 rounded-md'><BsCloudDownloadFill /> Resume</button>
+                <button className='flex items-center text-white gap-2 mt-[2vh] text-[11px] bg-[#28045C] py-1 px-3 rounded-md'><BsCloudDownloadFill /> Resume</button>
             </div>
-            <div className='mt-[32vh]'>
+            <div className='mt-[25vh]'>
                 <div className='text-left flex flex-col'>
                 <p className='text-white'>I'm a</p>
                 <h1 className='text-[#FFD73A] font-black text-[40px]'>Web Developer_</h1>
                 <p className='text-white text-[12px] pl-1'>3+ Years experience in Web Development</p>
                 <div className='flex mt-3 gap-3'>
-                    <button className='flex items-center text-[10px] text-white py-2 px-5 w-[150px] justify-between bg-[#28045C] rounded'>I need a website  <BsChevronRight className='text-right'/></button>
-                    <button className='flex items-center text-[10px] text-white py-2 px-5 w-[150px] justify-between bg-[#7E28FF] rounded'>I'm looking to hire  <BsChevronRight className='text-right'/></button>
+                    <button className='flex items-center text-[10px] text-white py-2 px-5 w-[150px] justify-between bg-[#28045C] rounded hover:rounded-lg'>I need a website  <BsChevronRight className='text-right'/></button>
+                    <button className='flex items-center text-[10px] text-white py-2 px-5 w-[150px] justify-between bg-[#7E28FF] rounded hover:rounded-lg'>I'm looking to hire  <BsChevronRight className='text-right'/></button>
                 </div>
                 </div>
             </div>
             </div>
-            <img src={Me} alt="" className='h-[60vh] right-0 pr-[170px] z-20 mt-[18vh]'/>
+            <img src={Me} alt="" className='h-[60vh] right-0 pr-[170px] z-20 mt-[10vh]'/>
         </div>
-        <img src={Coding} alt="" className='absolute top-[135px] right-[130px] z-10' />
+        <img src={Coding} alt="" className='absolute top-[15vh] right-[130px] z-10' />
     
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
-
+import { NavLink} from 'react-router-dom'
 export default function Navbar() {
   return (
         <div className='flex justify-between items-center h-[8vh] w-screen text-white px-5'>
             <div>
                 <h5 style={{letterSpacing: "5px"}}>Jake.</h5>
             </div>
-            <div className='flex justify-between text-[11px] gap-14 pr-10'>
-                <h1 className='py-1 px-2'>Home</h1>
-                <h1 className='py-1 px-2'>Projects</h1>
-                <h1 className='py-1 px-2'>About</h1>
-                <h1 className='py-1 px-2'>Contact</h1>
-            </div>
+            <ul className='flex justify-between text-[11px] gap-14 pr-10'>
+                <li className=' cursor-pointer hover:text-purple-100 '><NavLink exact="true" activeclassname="active" to="/" end> Home </NavLink></li>
+                <li className=' cursor-pointer hover:text-purple-100 '><NavLink exact="true" activeclassname="active" to="/projects" end> Projects </NavLink></li>
+                <li className=' cursor-pointer hover:text-purple-100 '><NavLink exact="true" activeclassname="active" to="/about" end> About </NavLink></li>
+                <li className=' cursor-pointer hover:text-purple-100 '><NavLink exact="true" activeclassname="active" to="/contact" end> Contact </NavLink></li>
+            </ul>
         </div>
   )
 }
