fcaf3fd682f1dcc0ce795dd7a361a47eb8029fcd
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-10 17:52:29 +0800

    contact footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,9 @@ export default function Footer() {
       <h1 className='text-center text-white text-[18px] font-medium' style={{letterSpacing:"3px"}}>CONTACT JAKE</h1>
       <div className='h-auto w-screen bg-[#6905F9] rounded-tr-[12rem] rounded-tl-[9rem] flex flex-col justify-center items-center gap-2 py-5'>
         <div className='flex gap-4 text-white'>
-          <BsFacebook fontSize="16px"/>
-          <BsTwitter fontSize="16px"/>
-          <BsTelegram fontSize="16px"/>
-          <BsGithub fontSize="16px"/>
+          <a href="https://www.facebook.com/jakedsi/" target="_blank"><BsFacebook fontSize="16px"/></a>
+          <a href="https://twitter.com/AboyotJake" target="_blank"><BsTwitter fontSize="16px"/></a>
+          <a href="https://github.com/jakedsi" target="_blank"><BsGithub fontSize="16px"/></a>
         </div>
         <h5 className='text-[13px] text-white'>or</h5>
         <div className='text-white flex gap-3'>
