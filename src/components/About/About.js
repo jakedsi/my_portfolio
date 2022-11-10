cf7192f330566f2b4766ceef62920ad15e978a9d
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-10 13:59:17 +0800

    responsive project route/page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='h-auto'>
         <h1 className='text-center text-white text-[20px] font-medium my-[5vh]' style={{letterSpacing:"3px"}}>ABOUT JAKE</h1>
-        <p className='w-screen text-center px-[22vw] mt-[11vh] text-[15px] text-[#c1c1c1]'>Hello! My name is Jake, and currently living in Philippines as a web developer. Graduated in University of Mindanao with the course of BS in Information Technology I primarily use PHP, but picking up a new framework or language isn't a problem. I have extensive programming experience and I'm comfortable developing on the frontend or backend web development</p>
+        <p className='w-screen text-center px-[22vw] mt-[11vh] text-[15px] text-[#c1c1c1] md:px-[10vw]'>Hello! My name is Jake, and currently living in Philippines as a web developer. Graduated in University of Mindanao with the course of BS in Information Technology I primarily use PHP, but picking up a new framework or language isn't a problem. I have extensive programming experience and I'm comfortable developing on the frontend or backend web development</p>
         <h1 className='text-center text-[#FFD63C] text-[15px] font-medium mt-[10vh]' style={{letterSpacing:"3px"}}>Backend Development Experience</h1>
         <p className='w-screen text-center px-[22vw] mt-[2vh] text-[14px] text-[#c1c1c1]'>PHP Native, Codeigniter Framework and ExpressJS for building my RESTful API</p>
         <h1 className='text-center text-[#FFD63C] text-[15px] font-medium mt-[10vh]' style={{letterSpacing:"3px"}}>Frontend Development Experience</h1>
