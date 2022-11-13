fa4d1914e15b6a3a499e327334aabc2faa656984
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-13 19:43:47 +0800

    add linkedin contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {BsFacebook, BsTwitter, BsGithub} from 'react-icons/bs'
-import {AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineMail, AiFillLinkedin} from 'react-icons/ai'
 export default function Footer() {
   return (
     <div className=' mt-[20vh]'>
       <h1 className='text-center text-white text-[18px] font-medium' style={{letterSpacing:"3px"}}>CONTACT JAKE</h1>
       <div className='h-auto w-screen bg-[#6905F9] rounded-tr-[12rem] rounded-tl-[9rem] flex flex-col justify-center items-center gap-2 py-5'>
         <div className='flex gap-4 text-white'>
+          <a href="https://www.linkedin.com/in/jake-aboyot-8b89b9189/" target="_blank" rel="noreferrer"><AiFillLinkedin fontSize="16px"/></a>
           <a href="https://www.facebook.com/jakedsi/" target="_blank" rel="noreferrer"><BsFacebook fontSize="16px"/></a>
           <a href="https://twitter.com/AboyotJake" target="_blank" rel="noreferrer"><BsTwitter fontSize="16px"/></a>
           <a href="https://github.com/jakedsi" target="_blank" rel="noreferrer"><BsGithub fontSize="16px"/></a>
